66996d404460935559feaac3043968769551b476
Your Full Name <timobest121@gmail.com>
2025-09-18 16:57:57 +0100

    besrt

--- src/Adejoke.tsx
+++ src/Adejoke.tsx
@@ -2,217 +2,226 @@ const Adejoke = () => {
   return (
     <div className="bg-black p-8 h-full">
       <div>
-        <h1 className="text-white text-5xl">Hi Sunshine ☀️😍❤️</h1>
+        <h1 className="text-white text-4xl">Hi Sunshine ☀️😍❤️</h1>
         <p className="text-white text-xl">Good morning</p>
         <p className="text-white text-xl">Did you sleep well?</p>
 
         <div className="mt-4">
           <p className="text-white text-xl">And yeah</p>
-          <p className="text-xl text-white">
+          <p className="text-l text-white">
             I want to give you real forhead-kiss so bad 🫂🥹!!!!
           </p>
-          <p className="text-xl text-white">
+          <p className="text-l text-white">
             I want to give you real forhead-kiss so bad 🫂🥹!!!!
           </p>
-          <p className="text-xl text-white">
+          <p className="text-l text-white">
             I want to give you real forhead-kiss so bad 🫂🥹!!!!
           </p>
-          <p className="text-xl text-white">
+          <p className="text-l text-white">
             I want to give you real forhead-kiss so bad 🫂🥹!!!!
           </p>
-          <p className="text-xl text-white">
+          <p className="text-l text-white">
             I want to give you real forhead-kiss so bad 🫂🥹!!!!
           </p>
-          <p className="text-xl text-white">
+          <p className="text-l text-white">
             I want to give you real forhead-kiss so bad 🫂🥹!!!!
           </p>
-          <p className="text-xl text-white">
+          <p className="text-l text-white">
             I want to give you real forhead-kiss so bad 🫂🥹!!!!
           </p>
-          <p className="text-xl text-white">
+          <p className="text-l text-white">
             I want to give you real forhead-kiss so bad 🫂🥹!!!!
           </p>
-          <p className="text-xl text-white">
+          <p className="text-l text-white">
             I want to give you real forhead-kiss so bad 🫂🥹!!!!
           </p>
-          <p className="text-xl text-white">
+          <p className="text-l text-white">
             I want to give you real forhead-kiss so bad 🫂🥹!!!!
           </p>
-          <p className="text-xl text-white">
+          <p className="text-l text-white">
             I want to give you real forhead-kiss so bad 🫂🥹!!!!
           </p>
-          <p className="text-xl text-white">
+          <p className="text-l text-white">
             I want to give you real forhead-kiss so bad 🫂🥹!!!!
           </p>
-          <p className="text-xl text-white">
+          <p className="text-l text-white">
             I want to give you real forhead-kiss so bad 🫂🥹!!!!
           </p>
-          <p className="text-xl text-white">
+          <p className="text-l text-white">
             I want to give you real forhead-kiss so bad 🫂🥹!!!!
           </p>
-          <p className="text-xl text-white">
+          <p className="text-l text-white">
             I want to give you real forhead-kiss so bad 🫂🥹!!!!
           </p>
-          <p className="text-xl text-white">
+          <p className="text-l text-white">
             I want to give you real forhead-kiss so bad 🫂🥹!!!!
           </p>
-          <p className="text-xl text-white">
+          <p className="text-l text-white">
             I want to give you real forhead-kiss so bad 🫂🥹!!!!
           </p>
-          <p className="text-xl text-white">
+          <p className="text-l text-white">
             I want to give you real forhead-kiss so bad 🫂🥹!!!!
           </p>
-          <p className="text-xl text-white">
+          <p className="text-l text-white">
             I want to give you real forhead-kiss so bad 🫂🥹!!!!
           </p>
-          <p className="text-xl text-white">
+          <p className="text-l text-white">
             I want to give you real forhead-kiss so bad 🫂🥹!!!!
           </p>
-          <p className="text-xl text-white">
+          <p className="text-l text-white">
             I want to give you real forhead-kiss so bad 🫂🥹!!!!
           </p>
-          <p className="text-xl text-white">
+          <p className="text-l text-white">
             I want to give you real forhead-kiss so bad 🫂🥹!!!!
           </p>
-          <p className="text-xl text-white">
+          <p className="text-l text-white">
             I want to give you real forhead-kiss so bad 🫂🥹!!!!
           </p>
-          <p className="text-xl text-white">
+          <p className="text-l text-white">
             I want to give you real forhead-kiss so bad 🫂🥹!!!!
           </p>
-          <p className="text-xl text-white">
+          <p className="text-l text-white">
             I want to give you real forhead-kiss so bad 🫂🥹!!!!
           </p>
-          <p className="text-xl text-white">
+          <p className="text-l text-white">
             I want to give you real forhead-kiss so bad 🫂🥹!!!!
           </p>
-          <p className="text-xl text-white">
+          <p className="text-l text-white">
             I want to give you real forhead-kiss so bad 🫂🥹!!!!
           </p>
-          <p className="text-xl text-white">
+          <p className="text-l text-white">
             I want to give you real forhead-kiss so bad 🫂🥹!!!!
           </p>
-          <p className="text-xl text-white">
+          <p className="text-l text-white">
             I want to give you real forhead-kiss so bad 🫂🥹!!!!
           </p>
-          <p className="text-xl text-white">
+          <p className="text-l text-white">
             I want to give you real forhead-kiss so bad 🫂🥹!!!!
           </p>
-          <p className="text-xl text-white">
+          <p className="text-l text-white">
             I want to give you real forhead-kiss so bad 🫂🥹!!!!
           </p>
-          <p className="text-xl text-white">
+          <p className="text-l text-white">
             I want to give you real forhead-kiss so bad 🫂🥹!!!!
           </p>
-          <p className="text-xl text-white">
+          <p className="text-l text-white">
             I want to give you real forhead-kiss so bad 🫂🥹!!!!
           </p>
-          <p className="text-xl text-white">
+          <p className="text-l text-white">
             I want to give you real forhead-kiss so bad 🫂🥹!!!!
           </p>
-          <p className="text-xl text-white">
+          <p className="text-l text-white">
             I want to give you real forhead-kiss so bad 🫂🥹!!!!
           </p>
-          <p className="text-xl text-white">
+          <p className="text-l text-white">
             I want to give you real forhead-kiss so bad 🫂🥹!!!!
           </p>
-          <p className="text-xl text-white">
+          <p className="text-l text-white">
             I want to give you real forhead-kiss so bad 🫂🥹!!!!
           </p>
-          <p className="text-xl text-white">
+          <p className="text-l text-white">
             I want to give you real forhead-kiss so bad 🫂🥹!!!!
           </p>
-          <p className="text-xl text-white">
+          <p className="text-l text-white">
             I want to give you real forhead-kiss so bad 🫂🥹!!!!
           </p>
-          <p className="text-xl text-white">
+          <p className="text-l text-white">
             I want to give you real forhead-kiss so bad 🫂🥹!!!!
           </p>
-          <p className="text-xl text-white">
+          <p className="text-l text-white">
             I want to give you real forhead-kiss so bad 🫂🥹!!!!
           </p>
-          <p className="text-xl text-white">
+          <p className="text-l text-white">
             I want to give you real forhead-kiss so bad 🫂🥹!!!!
           </p>
-          <p className="text-xl text-white">
+          <p className="text-l text-white">
             I want to give you real forhead-kiss so bad 🫂🥹!!!!
           </p>
-          <p className="text-xl text-white">
+          <p className="text-l text-white">
             I want to give you real forhead-kiss so bad 🫂🥹!!!!
           </p>
-          <p className="text-xl text-white">
+          <p className="text-l text-white">
             I want to give you real forhead-kiss so bad 🫂🥹!!!!
           </p>
-          <p className="text-xl text-white">
+          <p className="text-l text-white">
             I want to give you real forhead-kiss so bad 🫂🥹!!!!
           </p>
-          <p className="text-xl text-white">
+          <p className="text-l text-white">
             I want to give you real forhead-kiss so bad 🫂🥹!!!!
           </p>
-          <p className="text-xl text-white">
+          <p className="text-l text-white">
             I want to give you real forhead-kiss so bad 🫂🥹!!!!
           </p>
-          <p className="text-xl text-white">
+          <p className="text-l text-white">
             I want to give you real forhead-kiss so bad 🫂🥹!!!!
           </p>
-          <p className="text-xl text-white">
+          <p className="text-l text-white">
             I want to give you real forhead-kiss so bad 🫂🥹!!!!
           </p>
-          <p className="text-xl text-white">
+          <p className="text-l text-white">
             I want to give you real forhead-kiss so bad 🫂🥹!!!!
           </p>
-          <p className="text-xl text-white">
+          <p className="text-l text-white">
             I want to give you real forhead-kiss so bad 🫂🥹!!!!
           </p>
-          <p className="text-xl text-white">
+          <p className="text-l text-white">
             I want to give you real forhead-kiss so bad 🫂🥹!!!!
           </p>
-          <p className="text-xl text-white">
+          <p className="text-l text-white">
             I want to give you real forhead-kiss so bad 🫂🥹!!!!
           </p>
-          <p className="text-xl text-white">
+          <p className="text-l text-white">
             I want to give you real forhead-kiss so bad 🫂🥹!!!!
           </p>
-          <p className="text-xl text-white">
+          <p className="text-l text-white">
             I want to give you real forhead-kiss so bad 🫂🥹!!!!
           </p>
-          <p className="text-xl text-white">
+          <p className="text-l text-white">
             I want to give you real forhead-kiss so bad 🫂🥹!!!!
           </p>
-          <p className="text-xl text-white">
+          <p className="text-l text-white">
             I want to give you real forhead-kiss so bad 🫂🥹!!!!
           </p>
-          <p className="text-xl text-white">
+          <p className="text-l text-white">
             I want to give you real forhead-kiss so bad 🫂🥹!!!!
           </p>
-          <p className="text-xl text-white">
+          <p className="text-l text-white">
             I want to give you real forhead-kiss so bad 🫂🥹!!!!
           </p>
-          <p className="text-xl text-white">
+          <p className="text-l text-white">
             I want to give you real forhead-kiss so bad 🫂🥹!!!!
           </p>
-          <p className="text-xl text-white">
+          <p className="text-l text-white">
             I want to give you real forhead-kiss so bad 🫂🥹!!!!
           </p>
-          <p className="text-xl text-white">
+          <p className="text-l text-white">
             I want to give you real forhead-kiss so bad 🫂🥹!!!!
           </p>
-          <p className="text-xl text-white">
+          <p className="text-l text-white">
             I want to give you real forhead-kiss so bad 🫂🥹!!!!
           </p>
-          <p className="text-xl text-white">
+          <p className="text-l text-white">
             I want to give you real forhead-kiss so bad 🫂🥹!!!!
           </p>
-          <p className="text-xl text-white">
+          <p className="text-l text-white">
             I want to give you real forhead-kiss so bad 🫂🥹!!!!
           </p>
-          <p className="text-xl text-white">
+          <p className="text-l text-white">
             I want to give you real forhead-kiss so bad 🫂🥹!!!!
           </p>
-          <p className="text-xl text-white">
+          <p className="text-l text-white">
             I want to give you real forhead-kiss so bad 🫂🥹!!!!
           </p>
-          <p className="text-xl text-white">
+          <p className="text-l text-white">
+            I want to give you real forhead-kiss so bad 🫂🥹!!!!
+          </p>
+          <p className="text-l text-white">
+            I want to give you real forhead-kiss so bad 🫂🥹!!!!
+          </p>
+          <p className="text-l text-white">
+            I want to give you real forhead-kiss so bad 🫂🥹!!!!
+          </p>
+          <p className="text-l text-white">
             I want to give you real forhead-kiss so bad 🫂🥹!!!!
           </p>
         </div>
